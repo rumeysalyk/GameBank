c8c19055f4d1df94002a46e52e74b58636d647b0
rumeysalyk <layikrumeysa@gmail.com>
2022-11-20 16:44:28 +0300

    Ballons are created in random positions

--- BurstBalloons/BurstBaloons/Assets/Scripts/BaloonCreation.cs
+++ BurstBalloons/BurstBaloons/Assets/Scripts/BaloonCreation.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BaloonCreation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject m_balloon;
+
+    private float m_creationFreq = 1f;
+    private float m_timer = 0f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        m_timer -= Time.deltaTime;
+
+        if( m_timer < 0 )
+        {
+            var baloon = Instantiate( m_balloon, new Vector3(Random.Range(-2.4f, 2.4f), -5.5f, 0), Quaternion.Euler(0, 0, 0) ) as GameObject;
+            baloon.GetComponent<Rigidbody2D>().AddForce( new Vector2(0f, Random.Range(30f, 90f) ) );        
+            m_timer = m_creationFreq;
+        }
     }
+
 }
