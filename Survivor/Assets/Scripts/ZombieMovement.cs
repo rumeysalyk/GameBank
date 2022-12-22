265b6732c1fae7e6e9f5cb9c57c29d0ab96bc67f
rumeysalyk <layikrumeysa@gmail.com>
2022-12-22 23:43:09 +0300

    Zombie following user

---
+++ Survivor/Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class ZombieMovement : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find( "FPSController" );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = player.transform.position;
+    }
+}
