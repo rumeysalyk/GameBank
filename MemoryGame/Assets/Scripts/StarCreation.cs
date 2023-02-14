7d6cb85e0a0aaf048c6feb4fa907dcaabe746607
rumeysalyk <layikrumeysa@gmail.com>
2023-02-15 00:31:55 +0300

    Stars are created with Coroutine

--- MemoryGame/Assets/Scripts/StarCreation.cs
+++ MemoryGame/Assets/Scripts/StarCreation.cs
@@ -1,14 +1,13 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StarCreation : MonoBehaviour
 {
     public GameObject star;
     private GameObject obj;
     public GameObject m_starParent;
-
-    private float m_creationTime = 2f;
-    private float m_timeCounter = 0f;
-
+    public Image m_dialog;
     TMPro.TextMeshProUGUI m_numberText;
     public int level;
 
@@ -16,26 +15,37 @@ public class StarCreation : MonoBehaviour
     void Start()
     {
         obj = Instantiate(star, new Vector3(Random.Range( 115, Screen.width - 115 ), Random.Range( -145, Screen.height ), 0), Quaternion.identity, m_starParent.transform);
-
         m_numberText = obj.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+
+        startGame();
     }
 
-    void Update()
+    public void startGame()
     {
-        m_timeCounter -= Time.deltaTime;
-        if ( m_timeCounter < 0 )
-        {
-            m_numberText.SetText(Random.Range(10, 50).ToString());
+        level = 1;
 
-
-            float x = Random.Range(115, Screen.width-115);
-            float y = Random.Range(115, Screen.height - 115);
-
-
-            obj.transform.position = new Vector3(x, y, 0);
-            m_timeCounter = m_creationTime;
-        }
+        StartCoroutine( showStars( level + 1 ) );
     }
 
 
+    private IEnumerator showStars( int i )
+    {
+        while( i > 0 )
+        {
+            m_numberText.SetText( Random.Range( 10, 50 ).ToString() );
+
+            float x = Random.Range( 115, Screen.width - 115 );
+            float y = Random.Range( 115, Screen.height - 115 );
+
+            obj.transform.position = new Vector3( x, y, 0 );
+            Debug.Log(i);
+            yield return new WaitForSeconds( 1f );
+            i--;
+        }
+        m_dialog.gameObject.SetActive( true );
+        StopCoroutine( showStars( level + 1 ) );    
+        Time.timeScale = 0;
+        level++;
+        StartCoroutine( showStars( level + 1 ) );
+    }
 }
